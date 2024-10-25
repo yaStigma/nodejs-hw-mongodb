b82a8a1b6e30e5c674dd96259354918960b347c2
Tanya Yenakieva <yastigma@gmail.com>
2024-10-25 14:36:37 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import { initMongoConnection } from "./db/initMongoConnection.js";
 import { setupServer } from "./server.js";
 
 
 const boodstrap = async() =>{
+   await initMongoConnection();
    setupServer(); 
-}
+};
 
 boodstrap();

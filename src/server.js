7280cb75022da0717429e90db55c7f6794f68326
Tanya Yenakieva <yastigma@gmail.com>
2024-12-04 02:24:51 +0200

    swegger

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import authRouter from './routers/auth.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
-
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 export const setupServer = () => {
   const app = express();
 
@@ -26,6 +26,7 @@ export const setupServer = () => {
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.use('*', notFoundHandler);
 
   app.use(errorHandler);
